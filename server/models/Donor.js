0a1fa6923c7ea77ea38c4c66ab31e3347b5456b9
Nilesh Shinde <nileshshinde0431@gmail.com>
2026-05-11 19:07:29 +0530

    added donor for backend and mongodb connection

---
+++ server/models/Donor.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const donorSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    phone: {
+        type: String,
+        required: true
+    },
+
+    bloodGroup: {
+        type: String,
+        required: true
+    },
+
+    location: {
+        type: String,
+        required: true
+    }
+
+});
+
+module.exports = mongoose.model("Donor", donorSchema);
